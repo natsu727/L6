6098a3e2d64a5a463266eef233f2907a513fc36f
Ubuntu <ubuntu@ip-172-31-71-109.ec2.internal>
2024-10-29 19:01:09 +0000

    合計金額表示追加

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,11 @@
 class CartsController < ApplicationController
     def show
       @cart=CartItem.where(cart_id: current_cart.id)
-        # @cart = current_cart
+      
+      array=[]
+      @cart.each do |item|
+        array << item.qty * Product.find(item.product_id).price 
+      end
+      @total = array.sum
     end
 end
